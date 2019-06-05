e56517b8201d362be43bf64808a56137fa2d4c13
Ethan Miller <ethan.romans5.8@gmail.com>
2019-06-04 22:11:08 -0500

    replace wmi module with win32api for speed

--- Get_IMDB_Movie.py
+++ Get_IMDB_Movie.py
@@ -2,9 +2,9 @@
 # 3/13/2019
 
 from pyperclip import copy
-from win32api import GetVolumeInformation
+from win32api import GetVolumeInformation, GetLogicalDrives
+from win32file import GetDriveType
 from imdb import IMDb
-from wmi import WMI
 
 # Create an instance of the IMDb class.
 ia = IMDb()
@@ -47,29 +47,75 @@ def getImdbTitle(search):
     movie_name = f'{title} ({year})'
     return movie_name
 
-# Note: This function adds about 6 seconds to runtime. It is recomended that you remove it and simply set {letter} to your cdrom (e.g. replace letter = findCdRoms() with letter = 'D:')
-def findCdRoms():
+def getDrives():
   '''
-  Tests all of your disk drives to see whether
-  they are a cdrom or not and returns the item
-  selected from a list of drive letters that are cdroms.
+  Searches for local disk drives matching letters A-Z
+  that are in use and returns a list of the results.
   '''
-  # Get the drive letter of your DVD drive.
-  cdroms = WMI()
-  letters = []
-  for cdrom in cdroms.Win32_CDROMDrive():
-      letters.append(cdrom.Drive)
+  # Create an empty list to hold the drives.
+  drives = []
+  bitmask = GetLogicalDrives()
+  # Test for each possible drive letter.
+  for letter in list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'):
+    # If drive exists, append to list of drives.
+    if bitmask & 1:
+      drives.append(letter)
+    bitmask >>= 1
 
-  if len(letters) > 1:
+  return drives
+
+def getDriveTypes():
+  '''
+  Searches a list of disk drives and returns a
+  dictionary of the same list as keys with
+  drive type information as values.
+  '''
+  # Create an empty dictionary to hold the drive / drive types.
+  types = {}
+  # Create a list of drive types for typeIndex to match.
+  driveTypes = ['DRIVE_UNKNOWN', 'DRIVE_NO_ROOT_DIR', 'DRIVE_REMOVABLE', 'DRIVE_FIXED', 'DRIVE_REMOTE', 'DRIVE_CDROM', 'DRIVE_RAMDISK']
+
+  for drive in getDrives():
+    try:
+      # GetDriveType() will return a number that represents a drive type.
+      typeIndex = GetDriveType(u"%s:\\"%drive)
+      # Add new key / value pairs to the 'types' dictionary, where the drive letter is the key and the type is the value.
+      types[f'{drive}:'] = driveTypes[typeIndex]
+    except Exception as e:
+      print('Error: ', e)
+  return types
+
+def getCdRoms():
+  '''
+  Searches a dictionary of drives and
+  drive types and returns a list of
+  drives that are CD ROMs.
+  '''
+  drives = getDriveTypes()
+  cdroms = []
+  for drive, drive_type in drives.items():
+    if drive_type == 'DRIVE_CDROM':
+      cdroms.append(drive)
+  return cdroms
+
+def selectCdRom():
+  '''
+  Tests how many CD ROMs are in the machine,
+  and if more than 1, prompts the user to
+  select which CD ROM to use.
+  '''
+  cdroms = getCdRoms()
+
+  if len(cdroms) > 1:
     # Print the available drives.
-    for letter in letters:
-      print(f'{letters.index(letter)+1}. {letter}')
+    for letter in cdroms:
+      print(f'{cdroms.index(letter)+1}. {letter}')
     # Input validation.
     while True:
       try:
         # Promt user to select drive.
         index = input('Please select which drive to search (by number): ')
-        return letters[int(index)-1]
+        return cdroms[int(index)-1]
       except IndexError:
         print('Index error: That number doesn\'t appear to match any of the disk drives listed.')
         continue
@@ -80,11 +126,11 @@ def findCdRoms():
         print('Opps! Something went wrong. ', e)
         continue
   else:
-    return letters[0]
+    return cdroms[0]
 
-# Get the volume label of your cdrom.
-letter = findCdRoms()
-movie_name = getImdbTitle(getVolumeLabel(letter))
+# Get the volume label of your CD ROM.
+drive = selectCdRom()
+movie_name = getImdbTitle(getVolumeLabel(drive))
 
 # Remove special characters from string.
 movie_name = movie_name.translate({ord(i): None for i in '<>:"/\|?*'})
